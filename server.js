e5c9c9a72611e35e213d290ee846108c9c39ffbd
kiddspazz <travishorton.pianist@gmail.com>
2019-06-12 17:41:24 -0600

    added process.env.PORT back and also added http server back

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const http = require("http");
 const express = require("express");
 
 const app = express();
+const server = http.Server(app)
 const port = 8080;
 
 app.use(express.static("public"));
 
-app.listen(port, function(){
+server.listen(process.env.PORT || port, function(){
     console.log('Your node js server is running');
 });
