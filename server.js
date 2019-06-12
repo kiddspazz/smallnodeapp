6513cfe6c7832b23fd2db1ec5ce5fa8e1b67712c
kiddspazz <travishorton.pianist@gmail.com>
2019-06-12 17:53:57 -0600

    added dotenv to see what it does

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 #!/user/bin/env nodejs
 const http = require("http");
 const express = require("express");
+const dotenv = require("dotenv");
 
 const app = express();
 const server = http.Server(app)
 const port = 8080;
+
+dotenv.config();
+console.log(process.env.PORT);
 
 app.use(express.static("public"));
 
